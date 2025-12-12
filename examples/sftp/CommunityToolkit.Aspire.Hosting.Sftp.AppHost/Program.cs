12d1b7c1e228ba0f235e3ca601d6a50affde9ffc
Fabio <f.marini@gmail.com>
2025-12-12 18:25:54 +0100

    Added app host examples + removed SftpUser class

--- examples/sftp/CommunityToolkit.Aspire.Hosting.Sftp.AppHost/Program.cs
+++ examples/sftp/CommunityToolkit.Aspire.Hosting.Sftp.AppHost/Program.cs
@@ -3,8 +3,24 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args); ;
 
-var users = "foo:pass:::upload";
+// 1. pass users as args
+builder.AddSftp("sftp-1", port: 55010, args: "foo:pass:::uploads");
 
-builder.AddSftp("sftp", port: 55010, users);
+// 2. store users in config
+builder.AddSftp("sftp-2", port: 55020)
+    .WithBindMount(".\\etc\\sftp\\users.conf", "/etc/sftp/users.conf", isReadOnly: true);
+
+// 3. use own host key
+builder.AddSftp("sftp-3", port: 55030)
+    .WithBindMount(".\\etc\\sftp\\users.conf", "/etc/sftp/users.conf", isReadOnly: true)
+    .WithBindMount(".\\etc\\ssh\\ssh_host_ed25519_key", "/etc/ssh/sftp/ssh_host_ed25519_key", isReadOnly: true)
+    .WithBindMount(".\\etc\\ssh\\ssh_host_rsa_key", "/etc/ssh/sftp/ssh_host_rsa_key", isReadOnly: true);
+
+// 4. log in with private/public key pair (no pwd in users string)
+builder.AddSftp("sftp-4", port: 55040, args: "foo::::uploads")
+    //.WithBindMount(".\\etc\\ssh\\ssh_host_ed25519_key", "/etc/ssh/sftp/ssh_host_ed25519_key", isReadOnly: true)
+    //.WithBindMount(".\\etc\\ssh\\ssh_host_rsa_key", "/etc/ssh/sftp/ssh_host_rsa_key", isReadOnly: true)
+    .WithBindMount(".\\etc\\ssh\\ssh_host_rsa_key", "/home/foo/.ssh/keys/ssh_host_rsa_key", isReadOnly: true)
+    .WithBindMount(".\\etc\\ssh\\ssh_host_rsa_key.pub", "/home/foo/.ssh/keys/ssh_host_rsa_key.pub", isReadOnly: true);
 
 builder.Build().Run();
--- src/CommunityToolkit.Aspire.Hosting.Sftp/SftpHostingExtension.cs
+++ src/CommunityToolkit.Aspire.Hosting.Sftp/SftpHostingExtension.cs
@@ -3,39 +3,9 @@ using CommunityToolkit.Aspire.Hosting.Sftp;
 
 namespace Aspire.Hosting;
 
-// TODO: users from config and ssh keys are just volumes...
-// TODO: keep it simple and remove users class - can use hard-coded args for quick starts
+// TODO: test login with ssh keys (no pwd in users.conf)...
 // TODO: don't forget the missing xml comments!!!
-
-/// <summary>
-/// 
-/// </summary>
-/// <param name="Username"></param>
-/// <param name="Password"></param>
-/// <param name="IsEncrypted"></param>
-/// <param name="UserId"></param>
-/// <param name="GroupId"></param>
-/// <param name="Folders"></param>
-public record SftpUser(string Username, string Password, bool IsEncrypted = false, string? UserId = null, string? GroupId = null, string[]? Folders = null)
-{
-    /// <summary>
-    /// Returns a string that represents the current user credentials, including the username, password, user ID, group
-    /// ID, and folders.
-    /// </summary>
-    /// <remarks>If the password is encrypted, an ":e" suffix is appended to the password segment in the
-    /// returned string. This format can be used for serialization or logging purposes where a compact representation of
-    /// the credentials is required.</remarks>
-    /// <returns>A colon-delimited string containing the username, password (with an ":e" suffix if encrypted), user ID, group
-    /// ID, and folders.</returns>
-    public override string ToString()
-    {
-        var password = IsEncrypted ? $"{Password}:e" : Password;
-
-        var folders = Folders == null || Folders.Length == 0 ? "" : Folders.Aggregate((f1, f2) => $"{f1}, {f2}");
-
-        return $"{Username}:{password}:{UserId}:{GroupId}:{folders}";
-    }
-}
+// TODO: add docs and all examples to readme (explain what each etc file does)...
 
 /// <summary>
 /// Provides extension methods for adding an SFTP resource to an <see cref="IDistributedApplicationBuilder"/>.
@@ -47,40 +17,32 @@ public static class SftpHostingExtension
     /// </summary>
     /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/> to add the resource to.</param>
     /// <param name="name">The name of the resource.</param>
-    /// <param name="port">The SFTP portnumber for the atmoz SFTP container</param>
+    /// <param name="port">The SFTP port number for the atmoz SFTP container</param>
     /// <param name="args">Optional arguments for the atmoz SFTP container</param>
     /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
     public static IResourceBuilder<SftpContainerResource> AddSftp(this IDistributedApplicationBuilder builder,
         [ResourceName] string name,
         int? port = null,
-        string args = "")
+        string? args = null)
     {
         ArgumentNullException.ThrowIfNull("Service name must be specified.", nameof(name));
         SftpContainerResource resource = new(name);
 
-        IResourceBuilder<SftpContainerResource> rb = builder.AddResource(resource)
+        var resourceBuilder = builder.AddResource(resource)
             .WithImage(SftpContainerImageTags.Image)
             .WithImageTag(SftpContainerImageTags.Tag)
             .WithImageRegistry(SftpContainerImageTags.Registry)
-            .WithArgs(args)
             .WithEndpoint(targetPort: SftpContainerResource.SftpEndpointPort,
                 port: port,
                 name: SftpContainerResource.SftpEndpointName,
                 scheme: "sftp")
             ;
 
-        return rb;
-    }
+        if (!String.IsNullOrEmpty(args))
+        {
+            resourceBuilder.WithArgs(args);
+        }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="builder"></param>
-    /// <param name="users"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
-    public static IResourceBuilder<SftpContainerResource> WithUsers(this IResourceBuilder<SftpContainerResource> builder, SftpUser[] users)
-    {
-        throw new NotImplementedException();
+        return resourceBuilder;
     }
 }
