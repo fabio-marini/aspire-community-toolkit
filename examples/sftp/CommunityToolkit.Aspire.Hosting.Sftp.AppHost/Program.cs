19728613dd835305776793829e55b01306fad977
Fabio <f.marini@gmail.com>
2025-12-15 19:14:34 +0100

    Fixed "logging with ssh keys" scenario

--- examples/sftp/CommunityToolkit.Aspire.Hosting.Sftp.AppHost/Program.cs
+++ examples/sftp/CommunityToolkit.Aspire.Hosting.Sftp.AppHost/Program.cs
@@ -8,19 +8,21 @@ builder.AddSftp("sftp-1", port: 55010, args: "foo:pass:::uploads");
 
 // 2. store users in config
 builder.AddSftp("sftp-2", port: 55020)
-    .WithBindMount(".\\etc\\sftp\\users.conf", "/etc/sftp/users.conf", isReadOnly: true);
+    .WithBindMount(".\\etc\\sftp\\users.conf", "/etc/sftp/users.conf", isReadOnly: true)
+    ;
 
 // 3. use own host key
 builder.AddSftp("sftp-3", port: 55030)
     .WithBindMount(".\\etc\\sftp\\users.conf", "/etc/sftp/users.conf", isReadOnly: true)
-    .WithBindMount(".\\etc\\ssh\\ssh_host_ed25519_key", "/etc/ssh/sftp/ssh_host_ed25519_key", isReadOnly: true)
-    .WithBindMount(".\\etc\\ssh\\ssh_host_rsa_key", "/etc/ssh/sftp/ssh_host_rsa_key", isReadOnly: true);
+    .WithBindMount(".\\etc\\ssh\\ssh_host_ed25519_key", "/etc/ssh/ssh_host_ed25519_key")
+    .WithBindMount(".\\etc\\ssh\\ssh_host_rsa_key", "/etc/ssh/ssh_host_rsa_key")
+    ;
 
 // 4. log in with private/public key pair (no pwd in users string)
 builder.AddSftp("sftp-4", port: 55040, args: "foo::::uploads")
-    //.WithBindMount(".\\etc\\ssh\\ssh_host_ed25519_key", "/etc/ssh/sftp/ssh_host_ed25519_key", isReadOnly: true)
-    //.WithBindMount(".\\etc\\ssh\\ssh_host_rsa_key", "/etc/ssh/sftp/ssh_host_rsa_key", isReadOnly: true)
-    .WithBindMount(".\\etc\\ssh\\ssh_host_rsa_key", "/home/foo/.ssh/keys/ssh_host_rsa_key", isReadOnly: true)
-    .WithBindMount(".\\etc\\ssh\\ssh_host_rsa_key.pub", "/home/foo/.ssh/keys/ssh_host_rsa_key.pub", isReadOnly: true);
+    .WithBindMount(".\\etc\\ssh\\ssh_host_ed25519_key", "/etc/ssh/ssh_host_ed25519_key")
+    .WithBindMount(".\\etc\\ssh\\ssh_host_rsa_key", "/etc/ssh/ssh_host_rsa_key")
+    .WithBindMount(".\\home\\foo\\.ssh\\keys\\id_rsa.pub", "/home/foo/.ssh/keys/id_rsa.pub", isReadOnly: true)
+    ;
 
 builder.Build().Run();
