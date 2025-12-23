85f7c65258f99f570c0fea506e710ac30914beb6
Fabio <f.marini@gmail.com>
2025-12-23 17:11:57 +0100

    Added API service and tests

--- examples/sftp/CommunityToolkit.Aspire.Hosting.Sftp.AppHost/Program.cs
+++ examples/sftp/CommunityToolkit.Aspire.Hosting.Sftp.AppHost/Program.cs
@@ -3,31 +3,42 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-#region [Known Issues]
+// TODO: all app host tests are using the aspire integration test class fixture with the example project
+//       + all resource creation tests are using one TestDistributedApplicationBuilder per test and verifying the app model...
+//       + would really like to verify the client connectivity instead (same as KurrentDB)!
+// TODO: + remove all sftp resources from the example and refactor app host tests accordingly
+//       + refactor func tests to use new client and implement all the different scenarios there (not app host)
+// TODO: test all other extensions too, i.e. health check and disable tracing + client tests
 
-// TODO: how to use rsa host key file - if ed25519 not supplied, server will create one and use it...
+#region [All scenarios]
+
+// 1a. pass users as args
+//builder.AddSftp("sftp-1a", port: 55011).WithArgs($"foo:pass:::uploads");
+
+// 1b. pass users as environment variable
+//builder.AddSftp("sftp-1b", port: 55012).WithEnvironment("SFTP_USERS", "foo:pass:::uploads");
+
+// 1c. use encrypted passwords (generated with: `wsl mkpasswd -m sha-512`)
+//builder.AddSftp("sftp-1c", port: 55013).WithEnvironment("SFTP_USERS", "foo:$5$t9qxNlrcFqVBNnad$U27ZrjbKNjv4JkRWvi6MjX4x6KXNQGr8NTIySOcDgi4:e:::uploads");
+
+// 2. store users in config
+//builder.AddSftp("sftp-2", port: 55020).WithUsersFile(".\\etc\\sftp\\users.conf");
+
+// 3. use own host key
+//builder.AddSftp("sftp-3", port: 55030).WithUsersFile(".\\etc\\sftp\\users.conf").WithHostKeyFile(".\\etc\\ssh\\ssh_host_ed25519_key", KeyType.Ed25519);
+
+// 4a. log in with RSA private/public key pair (no pwd in users string)
+//builder.AddSftp("sftp-4a", port: 55041).WithArgs("foo::::uploads").WithUserKeyFile("foo", ".\\home\\foo\\.ssh\\keys\\id_rsa.pub", KeyType.Rsa);
+
+// 4b. log in with Ed25519 private/public key pair (no pwd in users string)
+//builder.AddSftp("sftp-4b", port: 55042).WithArgs("foo::::uploads").WithUserKeyFile("foo", ".\\home\\foo\\.ssh\\keys\\id_ed25519.pub", KeyType.Ed25519);
 
 #endregion
 
-// 1a. pass users as args
-builder.AddSftp("sftp-1a", port: 55011).WithArgs($"foo:pass:::uploads");
+var sftp = builder.AddSftp("sftp").WithEnvironment("SFTP_USERS", "foo:$5$t9qxNlrcFqVBNnad$U27ZrjbKNjv4JkRWvi6MjX4x6KXNQGr8NTIySOcDgi4:e:::uploads");
 
-// 1b. pass users as environment variable
-builder.AddSftp("sftp-1b", port: 55012).WithEnvironment("SFTP_USERS", "foo:pass:::uploads");
-
-// 1c. use encrypted passwords (generated with: `wsl mkpasswd -m sha-512`)
-builder.AddSftp("sftp-1c", port: 55013).WithEnvironment("SFTP_USERS", "foo:$5$t9qxNlrcFqVBNnad$U27ZrjbKNjv4JkRWvi6MjX4x6KXNQGr8NTIySOcDgi4:e:::uploads");
-
-// 2. store users in config
-builder.AddSftp("sftp-2", port: 55020).WithUsersFile(".\\etc\\sftp\\users.conf");
-
-// 3. use own host key
-builder.AddSftp("sftp-3", port: 55030).WithUsersFile(".\\etc\\sftp\\users.conf").WithHostKeyFile(".\\etc\\ssh\\ssh_host_ed25519_key", KeyType.Ed25519);
-
-// 4a. log in with RSA private/public key pair (no pwd in users string)
-builder.AddSftp("sftp-4a", port: 55041).WithArgs("foo::::uploads").WithUserKeyFile("foo", ".\\home\\foo\\.ssh\\keys\\id_rsa.pub", KeyType.Rsa);
-
-// 4b. log in with Ed25519 private/public key pair (no pwd in users string)
-builder.AddSftp("sftp-4b", port: 55042).WithArgs("foo::::uploads").WithUserKeyFile("foo", ".\\home\\foo\\.ssh\\keys\\id_ed25519.pub", KeyType.Ed25519);
+builder.AddProject<Projects.CommunityToolkit_Aspire_Hosting_Sftp_ApiService>("api")
+    .WithReference(sftp)
+    .WaitForStart(sftp);
 
 builder.Build().Run();
--- tests/CommunityToolkit.Aspire.Hosting.Sftp.Tests/AppHostTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.Sftp.Tests/AppHostTests.cs
@@ -1,10 +1,222 @@
+using Aspire.Hosting;
+using Aspire.Hosting.Utils;
 using CommunityToolkit.Aspire.Testing;
+using Microsoft.Extensions.Hosting;
 using Polly;
 using Projects;
 using Renci.SshNet;
 using Xunit.Abstractions;
 
 namespace CommunityToolkit.Aspire.Hosting.Sftp.Tests;
+
+public class SftpFunctionalTests : IDisposable
+{
+    private readonly IDistributedApplicationTestingBuilder builder;
+
+    private IResourceBuilder<SftpContainerResource>? resourceBuilder;
+    private DistributedApplication? distributedApplication;
+    private IHost? host;
+
+    public SftpFunctionalTests(ITestOutputHelper logger)
+    {
+        builder = TestDistributedApplicationBuilder.Create().WithTestAndResourceLogging(logger);
+    }
+
+    private async Task RunTestAsync(Action<HostApplicationBuilder> configure)
+    {
+        Assert.NotNull(resourceBuilder);
+
+        distributedApplication = builder.Build();
+
+        await distributedApplication.StartAsync();
+
+        var rns = distributedApplication.Services.GetRequiredService<ResourceNotificationService>();
+
+        try
+        {
+            await rns.WaitForResourceAsync(resourceBuilder.Resource.Name, "Running", new CancellationTokenSource(TimeSpan.FromSeconds(15)).Token);
+        }
+        catch
+        {
+            ResourceEvent? resourceEvent = null;
+
+            var res = rns.TryGetCurrentState(resourceBuilder.Resource.Name, out resourceEvent);
+
+            throw;
+        }
+
+        var hostBuilder = Host.CreateApplicationBuilder();
+
+        hostBuilder.Configuration[$"ConnectionStrings:{resourceBuilder.Resource.Name}"] = await resourceBuilder.Resource.ConnectionStringExpression.GetValueAsync(default);
+
+        configure(hostBuilder);
+
+        host = hostBuilder.Build();
+
+        await host.StartAsync();
+
+        var client = host.Services.GetRequiredService<SftpClient>();
+
+        try
+        {
+            var retryPolicy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(500));
+
+            var tokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(1));
+
+            await retryPolicy.ExecuteAsync(async () =>
+            {
+                await client.ConnectAsync(tokenSource.Token);
+            });
+        }
+        catch
+        {
+            throw;
+        }
+        finally
+        {
+            client.Disconnect();
+        }
+    }
+
+    [Fact]
+    public async Task VerifySftpResourceWithArgs()
+    {
+        resourceBuilder = builder
+            .AddSftp("sftp")
+            .WithArgs($"foo:pass:::uploads");
+
+        await RunTestAsync(bld =>
+        {
+            bld.AddSftpClient(resourceBuilder.Resource.Name, cfg =>
+            {
+                cfg.Username = "foo";
+                cfg.Password = "pass";
+            });
+        });
+    }
+
+    [Fact]
+    public async Task VerifySftpResourceWithUsersEnvironmentVariable()
+    {
+        resourceBuilder = builder
+            .AddSftp("sftp")
+            .WithEnvironment("SFTP_USERS", "foo:pass:::uploads");
+
+        await RunTestAsync(bld =>
+        {
+            bld.AddSftpClient(resourceBuilder.Resource.Name, cfg =>
+            {
+                cfg.Username = "foo";
+                cfg.Password = "pass";
+            });
+        });
+    }
+
+    [Fact]
+    public async Task VerifySftpResourceWithEncryptedPassword()
+    {
+        resourceBuilder = builder
+            .AddSftp("sftp")
+            .WithEnvironment("SFTP_USERS", "foo:$5$t9qxNlrcFqVBNnad$U27ZrjbKNjv4JkRWvi6MjX4x6KXNQGr8NTIySOcDgi4:e:::uploads");
+
+        await RunTestAsync(bld =>
+        {
+            bld.AddSftpClient(resourceBuilder.Resource.Name, cfg =>
+            {
+                cfg.Username = "foo";
+                cfg.Password = "pass";
+            });
+        });
+    }
+
+    [Fact]
+    public async Task VerifySftpResourceWithUsersFile()
+    {
+        resourceBuilder = builder
+            .AddSftp("sftp")
+            .WithUsersFile("users.conf");
+
+        await RunTestAsync(bld =>
+        {
+            bld.AddSftpClient(resourceBuilder.Resource.Name, cfg =>
+            {
+                cfg.Username = "foo";
+                cfg.Password = "pass";
+            });
+        });
+    }
+
+    [Fact]
+    public async Task VerifySftpResourceWithHostKey()
+    {
+        resourceBuilder = builder
+            .AddSftp("sftp")
+            .WithUsersFile("users.conf")
+            .WithHostKeyFile("ssh_host_ed25519_key", KeyType.Ed25519);
+
+        await RunTestAsync(bld =>
+        {
+            bld.AddSftpClient(resourceBuilder.Resource.Name, cfg =>
+            {
+                cfg.Username = "foo";
+                cfg.Password = "pass";
+            });
+        });
+    }
+
+    [Fact]
+    public async Task VerifySftpResourceWithRsaKeys()
+    {
+        resourceBuilder = builder
+            .AddSftp("sftp")
+            .WithArgs($"foo::::uploads")
+            .WithUserKeyFile("foo", "id_rsa.pub", KeyType.Rsa);
+
+        await RunTestAsync(bld =>
+        {
+            bld.AddSftpClient(resourceBuilder.Resource.Name, cfg =>
+            {
+                cfg.Username = "foo";
+                cfg.PrivateKeyFile = "id_rsa";
+            });
+        });
+    }
+
+    [Fact]
+    public async Task VerifySftpResourceWithEd25519Keys()
+    {
+        resourceBuilder = builder
+            .AddSftp("sftp")
+            .WithArgs($"foo::::uploads")
+            .WithUserKeyFile("foo", "id_ed25519.pub", KeyType.Ed25519);
+
+        await RunTestAsync(bld =>
+        {
+            bld.AddSftpClient(resourceBuilder.Resource.Name, cfg =>
+            {
+                cfg.Username = "foo";
+                cfg.PrivateKeyFile = "id_ed25519";
+            });
+        });
+    }
+
+    public void Dispose()
+    {
+        builder.Dispose();
+
+        if (distributedApplication is not null)
+        {
+            distributedApplication.Dispose();
+        }
+
+        if (host is not null)
+        {
+            host.Dispose();
+        }
+    }
+}
 
 public class AppHostTests(ITestOutputHelper log, AspireIntegrationTestFixture<CommunityToolkit_Aspire_Hosting_Sftp_AppHost> fix)
     : IClassFixture<AspireIntegrationTestFixture<CommunityToolkit_Aspire_Hosting_Sftp_AppHost>>
